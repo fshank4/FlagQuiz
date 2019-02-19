371eafda17c52622a5263e2fce79c5ad23d46e9f
fshank4 <fshank4@comcast.net>
2019-02-19 09:16:59 -0500

    Added code

--- app/src/main/java/com/example/efshan2659/flagquiz/MainActivityFragment.java
+++ app/src/main/java/com/example/efshan2659/flagquiz/MainActivityFragment.java
@@ -1,22 +1,82 @@
+// MainActivityFragment.java
+// Contains the Flag quiz logic
 package com.example.efshan2659.flagquiz;
 
-import android.support.v4.app.Fragment;
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.content.res.AssetManager;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.Fragment;
+import android.os.Handler;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewAnimationUtils;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
-/**
- * A placeholder fragment containing a simple view.
- */
 public class MainActivityFragment extends Fragment {
+    // String used when logging error messages
+    private static final String TAG = "FlagQuiz Activity";
 
-    public MainActivityFragment() {
-    }
+    private static final int FLAGS_IN_QUIZ = 10;
 
+    private List<String> fileNameList; // flag file names
+    private List<String> quizCountriesList; // countries in current quiz
+    private Set<String> regionsSet; // world regions in current quiz
+    private String correctAnswer; // number of correct guesses
+    private int totalGuesses; // number of guesses made
+    private int correctAnswers; // number of correct guesses
+    private int guessRows; // number of rows displaying guess buttons
+    private SecureRandom random; // used to randomize quiz
+    private Handler handler; // used to delay loading next flag
+    private Animation shakeAnimation; // animation for incorrect guess
+
+    private LinearLayout quizLinearLayout; // layout that contains the quiz
+    private TextView questionNumberTextView; // shows current question #
+    private ImageView flagImageView; // displays a flag
+    private LinearLayout[] guessLinearLayouts; // rows of answer buttons
+    private TextView answerTextView; // displays correct answer
+
+    // configures the MainActivityFragment when its View is created
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        super.onCreateView(inflater, container, savedInstanceState);
+        View view =
+            inflater.inflate(R.layout.fragment_main, container, false);
+
+        fileNameList = new ArrayList<>(); // diamond operator
+        quizCountriesList = new ArrayList<>();
+        random = new SecureRandom();
+        handler = new Handler();
+
+        // load the shake animation that's used for incorrect answers
+        shakeAnimation = AnimationUtils.loadAnimation(getActivity(),
+            R.anim.incorrect_shake);
+        shakeAnimation.setRepeatCount(3); // animation repeats 3 times
+
+        // get references to GUI components
+        return view;
     }
 }
