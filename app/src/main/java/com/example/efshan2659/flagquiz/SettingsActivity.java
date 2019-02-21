c10f9ddbb657e22eb52a919b310729940b11fdbe
Eliza <fshank4@comcast.net>
2019-02-21 15:05:09 -0500

    update SettingsActivity.java, SettingsActivityFragment.java, and deleted unused menu_settings.xml resource

--- app/src/main/java/com/example/efshan2659/flagquiz/SettingsActivity.java
+++ app/src/main/java/com/example/efshan2659/flagquiz/SettingsActivity.java
@@ -1,30 +1,19 @@
+// SettingsActivity.java
+// Activity to display SettingsActivityFragment on a phone
 package com.example.efshan2659.flagquiz;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 public class SettingsActivity extends AppCompatActivity {
-
+    // inflates the GUI, displays Toolbar and adds "up" button
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
-
 }
